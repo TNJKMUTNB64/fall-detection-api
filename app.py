ad66773335b6dcfd95abdb4d71156152665419f5
TNJKMUTNB64 <tnjkmutnb@gmail.com>
2025-05-19 21:49:24 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,36 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 import numpy as np
+import tensorflow.lite as tflite
 
 app = Flask(__name__)
 
+# โหลดโมเดล TFLite
+interpreter = tflite.Interpreter(model_path="fall_detection_model.tflite")
+interpreter.allocate_tensors()
 
-# โหลดโมเดลที่อัปโหลดมา
-model = tf.keras.models.load_model('fall_detection_model_Acc.keras')
-# model = tf.keras.models.load_model('fall_detection_model_Acc_cnn.keras')
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
     try:
         data = request.json.get('inputs')
-        
         if not data or len(data) != 3:
             return jsonify({"error": "Invalid input format. Expecting 3 values."}), 400
-        
-        # เติมลำดับเวลา
+
+        # เตรียม input shape (ซ้ำ 50 timestep)
         time_steps = 50
-        input_sequence = np.tile(data, (time_steps, 1))  # ทำซ้ำ 50 ครั้ง
+        input_sequence = np.tile(data, (time_steps, 1)).astype(np.float32)
         input_array = input_sequence.reshape(1, time_steps, 3)
 
-        # ทำการพยากรณ์
-        prediction = model.predict(input_array)
-        result = "Fall" if prediction[0][0] > 0.75 else "No Fall"
+        interpreter.set_tensor(input_details[0]['index'], input_array)
+        interpreter.invoke()
+        output = interpreter.get_tensor(output_details[0]['index'])
 
-        #return jsonify({"result": result})
+        result = "Fall" if output[0][0] > 0.75 else "No Fall"
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
